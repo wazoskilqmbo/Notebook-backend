7e8dc218b470345ae324d7974ade743b0f9617e3
Snehashish <wazoskilqmbo@gmail.com>
2023-07-22 13:42:54 +0530

    minor fix

--- controllers/user.js
+++ controllers/user.js
@@ -19,7 +19,7 @@ const CreateUser =  async(req,res)=>{
             }
         }
         const authToken = jwt.sign(data, process.env.JWT_SECRET);
-        res.status(201).json({ authToken });
+        return res.status(201).json({ authToken });
     } catch (error) {
         res.status(500).json({error:'msg:internal server error '})
         console.log(error)
@@ -45,7 +45,7 @@ const loginUser = async(req,res)=>{
         }
         const authToken = jwt.sign(data, process.env.JWT_SECRET);
 
-        res.status(201).send({authToken});
+        return res.status(201).send({authToken});
     } catch (error) {
         res.status(500).json({ error: 'msg:internal server error ' })
     }
